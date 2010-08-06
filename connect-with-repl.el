bc7dc34542d164b6dadfac46930fd5cc8d995cbd
trotr <snoalot+github@gmail.com>
2010-08-07 02:48:16 +0900

    using buffer instead of variable(ansync is buggy)

--- connect-with-repl.el
+++ connect-with-repl.el
@@ -10,61 +10,100 @@
   :type 'integer
   :group 'connect-with-repl)
 
-(defvar connect-with-repl-storage nil)	;; store history
-(defvar connect-with-repl-tmp-storage nil) ;; using for communication with repl
-(defvar %connect-with-repl-mutex% nil)
-(defvar %connect-with-repl-running-yetp% nil)
-(defvar connect-with-repl-filter-functions nil)
+(defcustom connect-with-repl-output-eval-result-p nil
+  "calling foo-eval function then output result or not at repl buffer"
+  :type 'boolean
+  :group 'connect-with-repl)
+  
+(defvar connect-with-repl-storage-buffer-name "* CWR storage *"
+  "storage buffer name")
+(defvar connect-with-repl-storage-buffer nil
+  "sotered buffer for connected repl's output") 
+(defvar %connect-with-repl-mutex% nil
+  "internal mutex variable (boolean)")
+(defvar connect-with-repl-filter-functions nil
+  "special variable this function manages internal mutex while running repl")
 
 ;;; utility
-(defmacro cnw-repl-available-when (&rest body)
-  `(if %connect-with-repl-mutex%
-       (message "associated repl is busy. ignored.")
-       (progn ,@body)))
+(defvar cnw-gensym-function nil)
+(defun cnw-gensym-function (&optional recreate?)
+  (if (or recreate? (not cnw-gensym-function))
+      (setq cnw-gensym-function (format "CNW:%s"  (gensym)))
+      (or cnw-gensym-function
+	  (cnw-gensym-function t))))
 
-(defmacro cnw-with-mutex (&rest body)
-  `(unwind-protect
-       (catch 'mutex
-	 (setq %connect-with-repl-mutex% t)
-	 ,@body)
-     (setq %connect-with-repl-mutex% nil)))
+(defmacro with-cnw-storage-buffer-bottom-at  (&rest action)
+  `(with-current-buffer connect-with-repl-storage-buffer
+    ;(save-excursion
+       (goto-char (point-max))
+       ,@action))
 
-(defmacro cnw-with-repl (filter-functions send-action &rest body)
-  `(cnw-with-mutex
-    (unwind-protect 
-	(let ((comint-preoutput-filter-functions ,filter-functions))
-	  (condition-case err
-	      (progn
-		(push connect-with-repl-tmp-storage connect-with-repl-storage)
-		(setq %connect-with-repl-running-yetp% t
-		      connect-with-repl-tmp-storage nil)
-		,send-action
-		,@body)
-	    (error (pop connect-with-repl-storage) (throw 'mutex err))))
-      (setq %connect-with-repl-running-yetp% nil))))
+(defun cnw-repl-finished-p ()
+  "if repl's action is finished, 
+so gensym-string is existed at end-of-buffer in storage-buffer"
+  (with-cnw-storage-buffer-bottom-at
+   (looking-back (cnw-gensym-function))))
+
+(defun connect-with-repl-output-result (&optional check-p)
+  (when (and check-p (not (cnw-repl-finished-p))) 
+    (error "connect-with-repl-output-result: repl is runnnig yet"))
+  (with-cnw-storage-buffer-bottom-at
+   (forward-line -1)
+   (goto-char (point-at-eol))
+   (buffer-substring (point-min) (point))))
+
+(defun cnw-storage-buffer-initialize ()
+  (unless connect-with-repl-storage-buffer
+    (setq connect-with-repl-storage-buffer
+	  (get-buffer-create connect-with-repl-storage-buffer-name)))
+  (with-current-buffer connect-with-repl-storage-buffer
+    (erase-buffer)))
+
+(defun cnw-starting-connect (proc string)
+  (cnw-gensym-function t) ;; update gensym-string  
+  (cnw-storage-buffer-initialize)
+  (connect-with-repl-send proc string))
+
+(defun cnw-wait-loop (timeout)
+  (let ((dt connect-with-repl-wait-time))
+    (if timeout
+    	(while (not (cnw-repl-finished-p))
+    	  (unless (> timeout 0) (error "time out"))
+    	  (sleep-for 0 dt) 
+    	  (decf timeout dt))
+    	(while (not (cnw-repl-finished-p))
+    	  (sleep-for 0 dt)))))
+
+(defun connect-with-repl (string proc &optional timeout check-p)
+  (declare (special comint-preoutput-filter-functions))
+  (let ((comint-preoutput-filter-functions connect-with-repl-filter-functions))
+    (cnw-starting-connect proc string)
+    (cnw-wait-loop timeout)
+    (connect-with-repl-output-result check-p)))
+
+(defvar connect-with-repl-ansync-function-timer nil)
+(defun connect-with-repl-ansync (string proc cont &optional check-p)
+  (declare (special comint-preoutput-filter-functions))
+  (let ((comint-preoutput-filter-functions connect-with-repl-filter-functions))
+    (cnw-starting-connect proc string)))
+    (lexical-let ((cont cont)
+		  (check-p check-p))
+      (setq connect-with-repl-ansync-function-timer
+	    (run-with-idle-timer 
+	     (* 0.001 connect-with-repl-wait-time) t
+	     (lambda ()
+	       (when (cnw-repl-finished-p)
+		 (funcall cont (connect-with-repl-output-result check-p))
+		 (cancel-timer connect-with-repl-ansync-function-timer))))))))
 
 (defun connect-with-repl-send (proc string)
   (comint-simple-send proc string))
 
-(defun connect-with-repl-internal (filters proc message &optional timeout)
-  (cnw-repl-available-when
-   (cnw-with-repl filters
-		  (connect-with-repl-send proc message)
-		  (if timeout
-		      (while %connect-with-repl-running-yetp%
-			(unless (> timeout 0) (error "time out"))
-			(sleep-for 0 connect-with-repl-wait-time)
-			(decf timeout 100))
-		      (while %connect-with-repl-running-yetp%
-			(sleep-for 0 connect-with-repl-wait-time)))
-		  (setq connect-with-repl-tmp-storage
-		  (mapconcat 'identity (nreverse connect-with-repl-tmp-storage) "")))))
-  
-(defun connect-with-repl (message &optional proc timeout filters) ;timeout is msec
-  (let ((proc (if (functionp proc) (funcall proc) proc))
-	(filters (or filters connect-with-repl-filter-functions)))
-    (connect-with-repl-internal filters proc message timeout)))
-
-(defun connect-with-repl-cleanup-storage () (interactive)
-  (setq connect-with-repl-storage nil))
-
+(defmacro connect-with-repl-filter-function-maker (prompt)
+  (let ((line (gensym)))
+    `(lambda (,line)
+       (with-cnw-storage-buffer-bottom-at
+	(insert ,line)
+	(when (string-match-p ,prompt ,line)
+	  (insert (cnw-gensym-function))))
+       ,(if connect-with-repl-output-eval-result-p line ""))))
---
+++ connect-with-repl.tmp.el
@@ -0,0 +1,78 @@
+(defvar connect-with-repl-storage nil)	;; store history
+(defvar connect-with-repl-tmp-storage nil) ;; using for communication with repl
+(defvar %connect-with-repl-mutex% nil)
+(defvar %connect-with-repl-running-yetp% nil)
+(defvar connect-with-repl-filter-functions nil)
+
+;;; utility
+(defmacro cnw-repl-available-when (&rest body)
+  `(if %connect-with-repl-mutex%
+       (message "associated repl is busy. ignored.")
+       (progn ,@body)))
+
+(defmacro cnw-with-mutex (&rest body)
+  `(unwind-protect
+       (catch 'mutex
+	 (setq %connect-with-repl-mutex% t)
+	 ,@body)
+     (setq %connect-with-repl-mutex% nil)))
+
+(defmacro cnw-with-repl (filter-functions send-action &rest body)
+  `(cnw-with-mutex
+    (unwind-protect 
+	(let ((comint-preoutput-filter-functions ,filter-functions))
+	  (condition-case err
+	      (progn
+		(push connect-with-repl-tmp-storage connect-with-repl-storage)
+		(setq %connect-with-repl-running-yetp% t
+		      connect-with-repl-tmp-storage nil)
+		,send-action
+		,@body)
+	    (error (pop connect-with-repl-storage) (throw 'mutex err))))
+      (setq %connect-with-repl-running-yetp% nil))))
+
+(defun connect-with-repl-send (proc string)
+  (comint-simple-send proc string))
+
+(defun connect-with-repl-internal (filters proc message &optional timeout)
+  (cnw-repl-available-when
+   (cnw-with-repl filters
+		  (connect-with-repl-send proc message)
+		  (if timeout
+		      (while %connect-with-repl-running-yetp%
+			(unless (> timeout 0) (error "time out"))
+			(sleep-for 0 connect-with-repl-wait-time)
+			(decf timeout 100))
+		      (while %connect-with-repl-running-yetp%
+			(sleep-for 0 connect-with-repl-wait-time)))
+		  (setq connect-with-repl-tmp-storage
+		  (mapconcat 'identity (nreverse connect-with-repl-tmp-storage) "")))))
+  
+(defun connect-with-repl (message &optional proc timeout filters) ;timeout is msec
+  (let ((proc (if (functionp proc) (funcall proc) proc))
+	(filters (or filters connect-with-repl-filter-functions)))
+    (connect-with-repl-internal filters proc message timeout)))
+
+(defun connect-with-repl-cleanup-storage () (interactive)
+  (setq connect-with-repl-storage nil))
+
+
+;; (cnw-repl-available-when
+;;  (cnw-with-repl (list (lambda (line)
+;; 			 (push line connect-with-repl-tmp-storage)
+;; 			(when (string-match "gosh>" line)
+;; 			  (setq %connect-with-repl-running-yetp% nil))
+;; 			line))
+;; 		(connect-with-repl-send (scheme-proc) "(+ 10 1)")
+;; 		(while %connect-with-repl-running-yetp%
+;; 		  (sleep-for 0 connect-with-repl-wait-time))
+;; 		(mapconcat 'identity connect-with-repl-tmp-storage "\n")))
+;; (cnw-with-mutex (+ 1 2))
+;; (connect-with-repl (funcall input-func))
+
+;;      (catch 'result
+;; 	  (while running-yet?
+;; 	    (unless (> timeout 0) (throw 'result "Time out: connect-with-repl-internal")) 
+;; 	    (sleep-for nil connect-with-repl-wait-time)
+;; 	    (decf timeout 100))
+;; 	  (mapconcat 'identity (setq connect-with-repl-storage (nreverse storage)))))))))
